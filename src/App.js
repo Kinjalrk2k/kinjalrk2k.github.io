c88ce41b1d2ea06b5fdff750283e95075aa29015
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2021-07-22 19:52:45 +0530

    refractored and changed random duration logoc

--- src/App.js
+++ src/App.js
@@ -1,75 +1,10 @@
 import React from "react";
 import "./App.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import socials from "./data/socials";
 
 class App extends React.Component {
   state = {
-    social: [
-      {
-        link: "https://github.com/Kinjalrk2k",
-        icon: <FontAwesomeIcon icon={["fab", "github"]} />,
-      },
-      {
-        link: "https://www.linkedin.com/in/kinjal-raykarmakar",
-        icon: <FontAwesomeIcon icon={["fab", "linkedin"]} />,
-      },
-      {
-        link: "https://www.facebook.com/kinjal.raykarmakar",
-        icon: <FontAwesomeIcon icon={["fab", "facebook"]} />,
-      },
-      {
-        link: "https://www.instagram.com/kinjal.raykarmakar",
-        icon: <FontAwesomeIcon icon={["fab", "instagram"]} />,
-      },
-      {
-        link: "https://twitter.com/kinjal2209",
-        icon: <FontAwesomeIcon icon={["fab", "twitter"]} />,
-      },
-      {
-        link: "https://www.quora.com/profile/Kinjal-Raykarmakar",
-        icon: <FontAwesomeIcon icon={["fab", "quora"]} />,
-      },
-      {
-        link: "https://profile.codersrank.io/user/kinjalrk2k",
-        icon: <span className="codersrank">&#xe91b;</span>,
-      },
-      {
-        link: "https://www.hackerrank.com/kinjalrk2k",
-        icon: <FontAwesomeIcon icon={["fab", "hackerrank"]} />,
-      },
-      {
-        link: "https://www.codechef.com/users/kinjalrk2k",
-        icon: <span className="codechef">&#xe900;</span>,
-      },
-      {
-        link: "https://stackoverflow.com/users/10537134/kinjal-raykarmakar",
-        icon: <FontAwesomeIcon icon={["fab", "stack-overflow"]} />,
-      },
-      {
-        link: "https://www.youtube.com/channel/UCINTZKi7pDBr_iDkibhTXVg/",
-        icon: <FontAwesomeIcon icon={["fab", "youtube"]} />,
-      },
-      {
-        link: "https://www.deviantart.com/kinjal2209",
-        icon: <FontAwesomeIcon icon={["fab", "deviantart"]} />,
-      },
-      {
-        link: "https://www.reddit.com/user/krk2k",
-        icon: <FontAwesomeIcon icon={["fab", "reddit"]} />,
-      },
-      // {
-      //   link: "https://in.pinterest.com/kinjalraykarmak",
-      //   icon: <FontAwesomeIcon icon={["fab", "pinterest"]} />,
-      // },
-      {
-        link: "https://dev.to/kinjalrk2k",
-        icon: <FontAwesomeIcon icon={["fab", "dev"]} />,
-      },
-      {
-        link: "https://www.credly.com/users/kinjalrk2k/badges",
-        icon: <span className="credly">&#xe901;</span>,
-      },
-    ],
+    social: socials,
   };
 
   constructor(props) {
@@ -79,20 +14,19 @@ class App extends React.Component {
   }
 
   renderSocial() {
-    let randDurations = new Set();
-    const socialLength = this.state.social.length;
-    while (randDurations.size !== socialLength) {
-      randDurations.add(Math.floor(Math.random() * socialLength) + 1);
-    }
-    randDurations = [...randDurations];
+    const randDurations = socials.map(
+      (s) => String(Math.random() * 3 + 1) + "s"
+    );
 
-    return this.state.social.map((s, i) => {
-      console.log(randDurations[i]);
+    // const animationDuration = "3s";
+
+    return socials.map((s, i) => {
+      // console.log(randDurations[i]);
       return (
         <div key={i}>
           <a
             className="fade-in"
-            style={{ animationDuration: randDurations[i] + "s" }}
+            style={{ animationDuration: randDurations[i] }}
             href={s.link}
           >
             {s.icon}
@@ -192,8 +126,8 @@ class App extends React.Component {
       window.addEventListener(
         "deviceorientation",
         (e) => {
-          var absolute = e.absolute;
-          var alpha = e.alpha;
+          // var absolute = e.absolute;
+          // var alpha = e.alpha;
           var beta = e.beta;
           var gamma = e.gamma;
 
@@ -210,7 +144,7 @@ class App extends React.Component {
           that.innerRef.current.style.msTransform = style;
           that.innerRef.current.style.oTransform = style;
 
-          console.log(betaMax);
+          // console.log(betaMax);
         },
         true
       );
@@ -227,7 +161,7 @@ class App extends React.Component {
                 id="profile"
                 onLoad={(e) => (e.target.className = "img-onLoad")}
                 alt="It's me!"
-                src="https://firebasestorage.googleapis.com/v0/b/portfolio-24dd4.appspot.com/o/images%2F1573102050331.jfif-1617201776341?alt=media&token=8c871b61-bb9c-4d3f-9392-2a503eb81795"
+                src="https://ik.imagekit.io/kinjal/1573102050331__1__Y3YJUgtKV.jpeg?updatedAt=1626932670644"
               ></img>
               <h1>KINJAL RAYKARMAKAR</h1>
             </div>
